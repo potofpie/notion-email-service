aec41b0a349e68c8526795ac211aed2d6f45f69d
Bobby Christopher <rec3college@gmail.com>
2022-06-27 01:13:16 -0400

    changed 404 page

--- fe/client/src/assets/index.tsx
+++ fe/client/src/assets/index.tsx
@@ -1,5 +1,7 @@
 import icon from './arroba.png'
 import NotFoundImage from './404.png'
+import NotFoundImageColor from './404-color.png'
+
 import VeryCool from './very_cool.gif'
 
 
@@ -7,5 +9,6 @@ import VeryCool from './very_cool.gif'
 export {
     icon,
     NotFoundImage,
+    NotFoundImageColor,
     VeryCool
 }
--- fe/client/src/components/NotFound.tsx
+++ fe/client/src/components/NotFound.tsx
@@ -1,7 +1,7 @@
 import {FC} from 'react';
 import { Link, } from "react-router-dom";
 import {Header} from './Header'
-import { NotFoundImage } from '../assets';
+import { NotFoundImageColor } from '../assets';
 
 import {
   TransparentButton
@@ -13,7 +13,7 @@ import {
       <div className='h-full flex flex-1 flex-col justify-between ' >
         <Header/>
         <div className='flex flex-col flex-1 justify-center items-center'>
-          <img className='h-1/3' src={NotFoundImage}/>
+          <img className='h-1/3' src={NotFoundImageColor}/>
           <Link  style={{ margin: 20}} to={`login`}> <TransparentButton style={{fontSize: 24}}> back 🏠 </TransparentButton></Link>
         </div>
       </div>   
