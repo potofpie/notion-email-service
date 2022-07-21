8c51e27f0c9b9f51d24c8e36a5cf7483b97d2a33
Bobby Christopher <rec3college@gmail.com>
2022-07-20 23:24:14 -0400

    dood it dude

--- api/app.py
+++ api/app.py
@@ -1,39 +1,35 @@
-import os
+from models.user_model import UserModel
+from apig_wsgi import make_lambda_handler
 from flask import Flask, jsonify, make_response, request, redirect
-from models import get_user
-from notion.oauth import notion_oauth
-from payment import create_checkout_session
 app = Flask(__name__)
 
 
-# payment
-app.add_url_rule('/create-checkout-session', view_func=create_checkout_session, methods=['GET'])
-
-# models
-app.add_url_rule('/users/<string:id>', view_func=get_user, methods=['GET'])
-# app.add_url_rule('/user', view_func=create_user, methods=['POST'])
-
-# Notion OAuth
-app.add_url_rule('/auth/notion/callback', view_func=notion_oauth, methods=['GET'])
 
 
-
-
-@app.route('/')
-def index():
+@app.route('/add_email', methods=['POST'])
+def add_email():
+    data = request.get_json()
+    try:
+        user_exists = UserModel.get(hash_key=id)
+    except:
+        print("User does not exist.")
+    user = UserModel(id='derp', email=data['email'])
+    user.save()
     return {'message': 'OK'}
 
 @app.errorhandler(404)
 def resource_not_found(e):
     return make_response(jsonify(error='Not found!'), 404)
 
+lambda_handler = make_lambda_handler(app)
+
+
 if __name__ == '__main__':
     import sys
     action = sys.argv[1]
     if(action == 'start'):
-        app.run(port=5000, debug=True,  ssl_context='adhoc')
+        app.run(port=5001, debug=True)
     elif(action == 'create_db'):
         from models import UserModel
         UserModel.create_table(read_capacity_units=1,write_capacity_units=1)
-
 
--- api/models/user_model.py
+++ api/models/user_model.py
@@ -1,7 +1,7 @@
 from pynamodb.attributes import NumberAttribute, UnicodeAttribute
 from pynamodb.models import Model
-from stripe import Subscription
-from models.encrypted_string_attribute import EncryptedStringAttribute
+# from stripe import Subscription
+# from models.encrypted_string_attribue import EncryptedStringAttribute
 
 
 
@@ -13,13 +13,8 @@ class UserModel(Model):
         table_name = 'users-table-dev'
     id = UnicodeAttribute(hash_key=True)
     email = UnicodeAttribute()
-    first_name = UnicodeAttribute(null=True)
-    last_name = UnicodeAttribute(null=True)
-    trial_email_count = NumberAttribute(default=100)
-    subscription_id = EncryptedStringAttribute(null=True)
-    notion_access_token = EncryptedStringAttribute(null=True)
 
 
-def get_user(id):
-    user = UserModel.get(hash_key=id)
-    return user.attribute_values
+# def get_user(id):
+#     user = UserModel.get(hash_key=id)
+#     return user.attribute_values
